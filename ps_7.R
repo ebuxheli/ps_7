e6b035895ca5d8a9f4f460f8c4e9e49bcc244f04
Enxhi Buxheli <enxhibuxheli@Enxhis-MacBook-Pro.local>
2018-11-18 11:30:20 -0500

    attached libraries and got upshot data

---
+++ ps_7.R
@@ -0,0 +1,32 @@
+# attaching libraries
+library(tidyverse)
+library(fs)
+library(dplyr)
+
+# getting upshot data
+download.file(url = "https://goo.gl/ZRCBda",
+              destfile = "upshot.zip",
+              quiet = TRUE,
+              mode = "wb")
+unzip("upshot.zip")
+
+# deleting upshot.zip once data loaded
+file_delete("upshot.zip")
+
+
+### consolidating upshot data (code taken from my midterm 2)
+# listing all of the file names for upshot data
+files_names <- dir_ls(path = "2018-live-poll-results-master/data/")
+
+# reading in all the csv files and extracting source info (state, wave, etc. )
+upshot <- map_dfr(files_names, read_csv, .id = "source") %>% 
+  mutate(source    = str_sub(source, 51, -5),
+         state     = toupper(str_sub(source, 1, 2)),
+         wave      = str_sub(source, -1, -1),
+         district  = paste0(state, "-", parse_number(str_sub(source, 2, 4))),
+         office    = case_when(str_sub(source, 3, 3) == "g" ~ "Governor",
+                               str_sub(source, 3, 3) == "s" ~ "Senate",
+                               str_sub(source, 3, 3) != c("s", "g") ~ "House"))
+
+
+
