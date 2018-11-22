3b2499a430b4bd7fd9a34177229a4fe861298b72
Enxhi Buxheli <enxhibuxheli@Enxhis-MacBook-Pro.local>
2018-11-22 09:58:08 -0500

    uploading the new data and shiny app

--- visual_weighting/app.R
+++ visual_weighting/app.R
@@ -27,8 +27,8 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      # selection bar with the party
-      selectInput("data_party", 
+      # checkbox with the party
+      checkboxGroupInput("data_party", 
                   h3("Party:"), 
                   choices = c("Democratic", "Republican")),
       
@@ -54,18 +54,41 @@ server <- function(input, output) {
     # filtering data from input
     flip_FILTER <- filter(input$data_office, file_party %in% c(input$data_party))
     
-    # plotting the filtered data using plotly
-    flip_FILTER %>% 
-      filter(metric == "Expected" | metric == "Actual") %>% 
-      plot_ly(x = ~district, y = ~percent, color = ~metric, type = "scatter", 
-              hoverinfo = 'text',
-              text = ~paste('District: ', district,
-                            '\n Percent: ', scales::percent(percent))) %>% 
-      layout(title = "2018 Midterm Elections: Republican Advantage",
-             xaxis = list(title = "District"),
-             yaxis = list(title = "Republican Advantage Percentage",
-                          tickformat = "%"))
-  })
+    ggplotly(tooltip = c("text"),
+             ggplot(data = flip_FILTER, 
+                    aes_string(x = state, y = vot_per, text = district, color = file_party)) + 
+               geom_point(position = "jitter") +
+               labs(x = "State", 
+                    y = "Democratic Advantage Flips", 
+                    title = paste("Percent of Flips in Each Voting District")) +
+               scale_color_manual(name = "Party", values = c("blue", "red"))) %>% 
+      config(displayModeBar = FALSE)
+  }) 
+  
+  output$about <- renderUI({
+    
+    # summary of the application and instructions (credit to Mrs. Lupion for the idea and some code)
+    str1 <- paste("Summary")
+    str2 <- paste("This application allows the user to see how party flips played a role in the election
+                  by allowing them to see which party the users flipped to. The Party shows which party
+                  the voters flipped from. For example, if the legend shows DEMOCRATIC that means the 
+                  there was a flip from Democratic to Republican voting based on their polling response.")
+    str3 <- paste("Source")
+    str4 <- paste("The New York Times Upshot/Sienna Poll and The New York Times Election Results Coverage")
+    
+    HTML(paste(h3(str1), p(str2), h3(str3), p(str4)))})
+    
+    # # plotting the filtered data using plotly
+    # flip_FILTER %>% 
+    #   filter(metric == "Expected" | metric == "Actual") %>% 
+    #   plot_ly(x = ~district, y = ~percent, color = ~metric, type = "scatter", 
+    #           hoverinfo = 'text',
+    #           text = ~paste('District: ', district,
+    #                         '\n Percent: ', scales::percent(percent))) %>% 
+    #   layout(title = "2018 Midterm Elections: Republican Advantage",
+    #          xaxis = list(title = "District"),
+    #          yaxis = list(title = "Republican Advantage Percentage",
+    #                       tickformat = "%"))
 }
 
 # Run the application 
